9eae3bac3bf7c06e075f58c6b30aa5b53034ab54
Randeep Singh <randeepsingh986@gmail.com>
2023-04-25 00:36:31 +0530

    ids

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -34,21 +34,21 @@ let Skills = () => {
     }
     return (
         <section className="skills-section" id="skills-section">
-            <div className="skills-card">
+            <div className="container">
                 <AnimationOnScroll animateIn="animate__fadeInLeft" animateOnce={true}>
                     <h3>Some technologies I've used:</h3>
                 </AnimationOnScroll>
                 <AnimationOnScroll animateIn="animate__fadeIn" animateOnce={true}>
-                    <div className="skill-logos-container skills-card-img ">
-                        <i className="skill-logo fa fa-html5 skills-card-name"></i>
-                        <i className="skill-logo fa fa-css3 skills-card-name"></i>
-                        <i className="skill-logo fab fa-js skills-card-name"></i>
-                        <i className="skill-logo fab fa-bootstrap skills-card-name"></i>
-                        <i className="skill-logo fab skills-card-name " style={boxStyle} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} ><SiRedux  /> </i>
-                        <i className="skill-logo fab skills-card-name " style={boxStyleMongo} onMouseEnter={handleMouseEnterMongo} onMouseLeave={handleMouseLeaveMongo} ><DiMongodb /></i>
-                        <i className="skill-logo fab fa-react skills-card-name"></i>
-                        <i className="skill-logo fab fa-node skills-card-name"></i>
-                        <i className="skill-logo fab fa-git-alt skills-card-name"></i>
+                    <div className="skill-logos-container">
+                        <i className="skill-logo fa fa-html5"></i>
+                        <i className="skill-logo fa fa-css3"></i>
+                        <i className="skill-logo fab fa-js"></i>
+                        <i className="skill-logo fab fa-bootstrap"></i>
+                        <i className="skill-logo fab " style={boxStyle} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} ><SiRedux  /> </i>
+                        <i className="skill-logo fab " style={boxStyleMongo} onMouseEnter={handleMouseEnterMongo} onMouseLeave={handleMouseLeaveMongo} ><DiMongodb /></i>
+                        <i className="skill-logo fab fa-react"></i>
+                        <i className="skill-logo fab fa-node"></i>
+                        <i className="skill-logo fab fa-git-alt"></i>
 
                     </div>
                 </AnimationOnScroll>
