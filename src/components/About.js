863a4ed228162293bf535b69e347aad8f626f706
Randeep Singh <randeepsingh986@gmail.com>
2023-04-28 19:56:35 +0530

    ids

--- src/components/About.js
+++ src/components/About.js
@@ -15,7 +15,7 @@ let About = () => {
                     <img src={profilePhoto} alt="profile" className="home-img" style={{borderRadius:"80%",width: "300px"}} />
                     <AnimationOnScroll animateIn="animate__fadeIn" animateOnce={true}>
                         <p id="user-detail-into" className="about-info" style={{textAlign:"justify"}}>
-                        I'm a Full-Stack Web Developer with the ability to learn and collaborate in rapidly changing environments and compositions. Worked through 1000+ hours of Bootcamp structure learning JavaScript, Node.Js, React.Js, MongoDB, Express, HTML5, and CSS3.
+                        Result-oriented and Enthusiastic Full Stack Web Developer proficient in tech Stacks like JavaScript, React Js, Node Js, Redux and MongoDB. Love to make Interactive UI's. I am a problem-solver and a quick learner who is constantly seeking new challenges and opportunities to grow and develop my skills. I am a highly motivated Full Stack Developer with good technical skills and a passion for creating innovative and effective solutions.
                         </p>
                         <p className="about-info" style={{marginTop: "-20px",textAlign:"justify"}}>
                         I am passionate about solving technical problems, researching and developing new sites and technologies. I undertake and challenge myself to always improve, by being proactive, diligent, responsible, and committed to finding the perfect solution to any problem.
