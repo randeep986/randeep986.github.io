6b8b498281bdf43f1a6b4a92a87a01eccd53b259
Randeep Singh <randeepsingh986@gmail.com>
2023-05-01 18:48:27 +0530

    ids

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -51,29 +51,98 @@ let Skills = () => {
                         <i className="skill-logo fab fa-git-alt"></i>
 
                     </div>
-                    <div style={{visibility:"hidden", display:"none"}}>
-                        <div className="skills-card">
-                            <img className="skills-card-img" src="" alt=""/>
-                            <p className="skils-card-name">h</p>
-                        </div>
-                        <div className="skills-card">
-                            <img className="skills-card-img" src="" alt=""/>
-                            <p className="skils-card-name">h</p>
-                        </div>                        <div className="skills-card">
-                            <img className="skills-card-img" src="" alt=""/>
-                            <p className="skils-card-name">h</p>
-                        </div>                        <div className="skills-card">
-                            <img className="skills-card-img" src="" alt=""/>
-                            <p className="skils-card-name">h</p>
-                        </div>                        <div className="skills-card">
-                            <img className="skills-card-img" src="" alt=""/>
-                            <p className="skils-card-name">h</p>
-                        </div>
-                    </div>
                 </AnimationOnScroll>
             </div>
         </section>
     )
 }
+
+  const skills = [
+    {
+      name: "Html 5",
+      icon: images.html,
+    },
+    {
+      name: "CSS 3",
+      icon: images.css,
+    },
+    {
+      name: "JavaScript",
+      icon: images.javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: images.typescript,
+    },
+    {
+      name: "React Js",
+      icon: images.react,
+    },
+    {
+      name: "Redux",
+      icon: images.redux,
+    },
+    {
+      name: "Node Js",
+      icon: images.node,
+    },
+    {
+      name: "Express",
+      icon: images.express,
+    },
+    {
+      name: "Sass",
+      icon: images.sass,
+    },
+    {
+      name: "Mongo DB",
+      icon: images.mongodb,
+    },
+    {
+      name: "Github",
+      icon: images.github,
+    },
+    {
+      name: "Chakra UI",
+      icon: images.chakra,
+    },
+    {
+      name: "Next Js",
+      icon: images.next,
+    },
+  ];
+
+  
+    <div id="skills">
+      <h2 className="head-text">
+        My <span>Skills</span>
+      </h2>
+
+      <div className="app__skills-container" >
+        <motion.div className="app__skills-list">
+          {skills.map((skill) => (
+            <motion.div
+              whileInView={{ opacity: [0, 1] }}
+              transition={{ duration: 0.5 }}
+              className="app__skills-item app__flex skills-card"
+              key={skill.name}
+            >
+              <div className="app__flex">
+                <motion.img
+                  whileHover={{ scale: [1, 1.2] }}
+                  src={skill.icon}
+                  alt={skill.name}
+                  className="skills-card-img"
+                />
+              </div>
+              <p className="p-text  skills-card-name">{skill.name}</p>
+            </motion.div>
+          ))}
+        </motion.div>
+      </div>
+    </div>
+  
+
+
 
 export default Skills;
