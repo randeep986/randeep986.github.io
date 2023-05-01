21ff08855f69c7e7405ebd95126c477ee1231a8b
Randeep Singh <randeepsingh986@gmail.com>
2023-05-01 22:38:04 +0530

    ids

--- src/components/About.js
+++ src/components/About.js
@@ -2,8 +2,19 @@ import React from "react";
 import profilePhoto from '../assets/images/profile-img.png';
 
 import { AnimationOnScroll } from 'react-animation-on-scroll';
+import { Button } from "@chakra-ui/react";
 
 let About = () => {
+    const abouts = {
+        title: "Full Stack Web Developer",
+        description:
+          "Result-oriented and Enthusiastic Full Stack Web Developer proficient in tech Stacks like JavaScript, React Js, Node Js, Redux and MongoDB. Love to make Interactive UI's. I am a problem-solver and a quick learner who is constantly seeking new challenges and opportunities to grow and develop my skills. I am a highly motivated Full Stack Developer with good technical skills and a passion for creating innovative and effective solutions.",
+      };
+      const handleClick = () =>{
+      window.open("https://drive.google.com/file/d/1IvwRAj-3hldcDAgkUlGL7KP3FmoBySuv/view?usp=sharing")
+      window.location.href='https://drive.google.com/uc?export=download&id=1IvwRAj-3hldcDAgkUlGL7KP3FmoBySuv'
+    }
+    
     return (
         <section className="about-section" id="about-section" style={{maginTop: "-10px"}}>
             <div className="container" >
@@ -14,12 +25,28 @@ let About = () => {
                     {/* //in img tag please replace the classname with "profile-photo"  */}
                     <img src={profilePhoto} alt="profile" className="home-img" style={{borderRadius:"80%",width: "300px"}} />
                     <AnimationOnScroll animateIn="animate__fadeIn" animateOnce={true}>
-                        <p id="user-detail-into" className="about-info" style={{textAlign:"justify"}}>
-                        Result-oriented and Enthusiastic Full Stack Web Developer proficient in tech Stacks like JavaScript, React Js, Node Js, Redux and MongoDB. Love to make Interactive UI's. I am a problem-solver and a quick learner who is constantly seeking new challenges and opportunities to grow and develop my skills. I am a highly motivated Full Stack Developer with good technical skills and a passion for creating innovative and effective solutions.
-                        </p>
-                        <p className="about-info" style={{marginTop: "-20px",textAlign:"justify"}}>
-                        I am passionate about solving technical problems, researching and developing new sites and technologies. I undertake and challenge myself to always improve, by being proactive, diligent, responsible, and committed to finding the perfect solution to any problem.
-                        </p>
+
+    <div className="about_container about section" id="about">
+      <div className="app__profiles">
+        <div className="app__profile-item">
+          <div className="app__profile-description">
+            <p className="p-text" style={{ marginTop: 10 }} id="user-detail-intro">
+            {abouts.description}
+            </p>
+            <Button
+              onClick={handleClick}
+              download
+              className="button-5"
+              id='resume-button-2'
+            >
+              Resume     
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  
+
                         
                     </AnimationOnScroll>
                 </div>
