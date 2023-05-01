1f3b9dbc08e0262de0562514ed7a8ecfd510759c
Randeep Singh <randeepsingh986@gmail.com>
2023-05-01 19:08:56 +0530

    ids

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -32,6 +32,9 @@ let Skills = () => {
         color: isHoverMongo ? "lightgreen"  : "black"
 
     }
+    const skills = [
+        
+    ]    
     return (
         <section className="skills-section" id="skills-section">
             <div className="container">
@@ -53,95 +56,43 @@ let Skills = () => {
                     </div>
                 </AnimationOnScroll>
             </div>
-        </section>
-    )
-}
+    
 
-  const skills = [
-    {
-      name: "Html 5",
-      icon: images.html,
-    },
-    {
-      name: "CSS 3",
-      icon: images.css,
-    },
-    {
-      name: "JavaScript",
-      icon: images.javascript,
-    },
-    {
-      name: "TypeScript",
-      icon: images.typescript,
-    },
-    {
-      name: "React Js",
-      icon: images.react,
-    },
-    {
-      name: "Redux",
-      icon: images.redux,
-    },
-    {
-      name: "Node Js",
-      icon: images.node,
-    },
-    {
-      name: "Express",
-      icon: images.express,
-    },
-    {
-      name: "Sass",
-      icon: images.sass,
-    },
-    {
-      name: "Mongo DB",
-      icon: images.mongodb,
-    },
-    {
-      name: "Github",
-      icon: images.github,
-    },
-    {
-      name: "Chakra UI",
-      icon: images.chakra,
-    },
-    {
-      name: "Next Js",
-      icon: images.next,
-    },
-  ];
-
-  
     <div id="skills">
       <h2 className="head-text">
         My <span>Skills</span>
       </h2>
 
       <div className="app__skills-container" >
-        <motion.div className="app__skills-list">
-          {skills.map((skill) => (
-            <motion.div
+        <div className="app__skills-list">
+          
+            <div
               whileInView={{ opacity: [0, 1] }}
               transition={{ duration: 0.5 }}
               className="app__skills-item app__flex skills-card"
-              key={skill.name}
+              key={skills}
             >
               <div className="app__flex">
-                <motion.img
+                <img
                   whileHover={{ scale: [1, 1.2] }}
-                  src={skill.icon}
-                  alt={skill.name}
+                  src={skills}
+                  alt={skills}
                   className="skills-card-img"
                 />
               </div>
-              <p className="p-text  skills-card-name">{skill.name}</p>
-            </motion.div>
-          ))}
-        </motion.div>
+              <p className="p-text  skills-card-name">{skills}</p>
+            </div>
+          
+        </div>
       </div>
     </div>
   
+
+
+        </section>
+    )
+    
+}
 
 
 
